60faa02643b326d79e8d84fe818011abf662d3cb
Gocarona <95749772+Gocarona@users.noreply.github.com>
2022-09-12 23:09:09 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -33,81 +33,81 @@ let questions = [
       "Avengers End Game"
     ]
   },
-    {
-    numb: 4,
-    question: " ",
-    answer: "Hello",
-    options: [
-      "Idharammayilatho",
-      "Calling sahasra",
-      "Tik Tik Tik",
-      "Hello"
-    ]
-  },
-     {
-    numb: 5,
-    question: " ",
-    answer: "Jai Lava Kusha",
-    options: [
-      "volcano",
-      "brother",
-      "Jai Lava Kusha",
-      "jayam"
-    ]
-  },
-  {
-    numb: 6,
-    question: " ",
-    answer: "Oopiri",
-    options: [
-      "Shyam Singha Roy",
-      "Kaashmora",
-      "Oopiri",
-      "Premam"
-    ]
-  },
-  {
-    numb: 7,
-    question: " ",
-    answer: "Fidha",
-    options: [
-      "Arya 2",
-      "Andhala Rakshasi",
-      "Fidha",
-      "Rx 100"
-    ]
-  },
-  {
-    numb: 8,
-    question: " ",
-    answer: "Age Of Ultron",
-    options: [
-      "Terminator",
-      "Robots",
-      "all of us dead",
-      "Age Of Ultron"
-    ]
-  },
-  {
-    numb: 9,
-    question: " ",
-    answer: "Paisa Vasool",
-    options: [
-      "Money money more money",
-      "Paisa Vasool",
-      "Kick",
-      "Khiladi"
-    ]
-  },
-  {
-    numb: 10,
-    question: " ",
-    answer: "Chandhamama Kathalu",
-    options: [
-      "c/o kancharapalem ",
-      "Moon Knight",
-      "Geethanjali",
-      "Chandhamama Kathalu"
-    ]
-  },
+//     {
+//     numb: 4,
+//     question: " ",
+//     answer: "Hello",
+//     options: [
+//       "Idharammayilatho",
+//       "Calling sahasra",
+//       "Tik Tik Tik",
+//       "Hello"
+//     ]
+//   },
+//      {
+//     numb: 5,
+//     question: " ",
+//     answer: "Jai Lava Kusha",
+//     options: [
+//       "volcano",
+//       "brother",
+//       "Jai Lava Kusha",
+//       "jayam"
+//     ]
+//   },
+//   {
+//     numb: 6,
+//     question: " ",
+//     answer: "Oopiri",
+//     options: [
+//       "Shyam Singha Roy",
+//       "Kaashmora",
+//       "Oopiri",
+//       "Premam"
+//     ]
+//   },
+//   {
+//     numb: 7,
+//     question: " ",
+//     answer: "Fidha",
+//     options: [
+//       "Arya 2",
+//       "Andhala Rakshasi",
+//       "Fidha",
+//       "Rx 100"
+//     ]
+//   },
+//   {
+//     numb: 8,
+//     question: " ",
+//     answer: "Age Of Ultron",
+//     options: [
+//       "Terminator",
+//       "Robots",
+//       "all of us dead",
+//       "Age Of Ultron"
+//     ]
+//   },
+//   {
+//     numb: 9,
+//     question: " ",
+//     answer: "Paisa Vasool",
+//     options: [
+//       "Money money more money",
+//       "Paisa Vasool",
+//       "Kick",
+//       "Khiladi"
+//     ]
+//   },
+//   {
+//     numb: 10,
+//     question: " ",
+//     answer: "Chandhamama Kathalu",
+//     options: [
+//       "c/o kancharapalem ",
+//       "Moon Knight",
+//       "Geethanjali",
+//       "Chandhamama Kathalu"
+//     ]
+//   },
 ];
