960334d5c4e69013d8fb5e5fd82e6daf56c8be62
Gocarona <95749772+Gocarona@users.noreply.github.com>
2022-09-13 21:26:56 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,113 +1,586 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: " ",
-    answer: "Arjun Reddy",
-    options: [
-      "Arjun Reddy",
-      "Andhari vadu",
-      "A1 Express",
-      "Aacharya"
-    ]
-  },
-    {
-    numb: 2,
-    question: " ",
-    answer: "Race Gurram",
-    options: [
-      "Gem",
-      "Race 2",
-      "Galodu",
-      "Race Gurram"
-    ]
-  },
-    {
-    numb: 3,
-    question: " ",
-    answer: "Avengers End Game",
-    options: [
-      "Terminator",
-      "The Avengers",
-      "Avengers Infinity War",
-      "Avengers End Game"
-    ]
-  },
-//     {
-//     numb: 4,
-//     question: " ",
-//     answer: "Hello",
-//     options: [
-//       "Idharammayilatho",
-//       "Calling sahasra",
-//       "Tik Tik Tik",
-//       "Hello"
-//     ]
-//   },
-//      {
-//     numb: 5,
-//     question: " ",
-//     answer: "Jai Lava Kusha",
-//     options: [
-//       "volcano",
-//       "brother",
-//       "Jai Lava Kusha",
-//       "jayam"
-//     ]
-//   },
-//   {
-//     numb: 6,
-//     question: " ",
-//     answer: "Oopiri",
-//     options: [
-//       "Shyam Singha Roy",
-//       "Kaashmora",
-//       "Oopiri",
-//       "Premam"
-//     ]
-//   },
-//   {
-//     numb: 7,
-//     question: " ",
-//     answer: "Fidha",
-//     options: [
-//       "Arya 2",
-//       "Andhala Rakshasi",
-//       "Fidha",
-//       "Rx 100"
-//     ]
-//   },
-//   {
-//     numb: 8,
-//     question: " ",
-//     answer: "Age Of Ultron",
-//     options: [
-//       "Terminator",
-//       "Robots",
-//       "all of us dead",
-//       "Age Of Ultron"
-//     ]
-//   },
-//   {
-//     numb: 9,
-//     question: " ",
-//     answer: "Paisa Vasool",
-//     options: [
-//       "Money money more money",
-//       "Paisa Vasool",
-//       "Kick",
-//       "Khiladi"
-//     ]
-//   },
-//   {
-//     numb: 10,
-//     question: " ",
-//     answer: "Chandhamama Kathalu",
-//     options: [
-//       "c/o kancharapalem ",
-//       "Moon Knight",
-//       "Geethanjali",
-//       "Chandhamama Kathalu"
-//     ]
-//   },
+  {
+  numb: 1,
+  question: " ",
+  answer: "Nuvve nuvve",
+  options: [
+    "Nuvvu nenu",
+    "Nuvvu naaku nachav",
+    "Nuvve nuvve",
+    "Nuvvila"
+  ]
+},
+  {
+  numb: 2,
+  question: " ",
+  answer: "Race Gurram",
+  options: [
+    "Gem",
+    "Race 2",
+    "Galodu",
+    "Race Gurram"
+  ]
+},
+  {
+  numb: 3,
+  question: " ",
+  answer: "Avengers End Game",
+  options: [
+    "Terminator",
+    "The Avengers",
+    "Avengers Infinity War",
+    "Avengers End Game"
+  ]
+},
+  {
+  numb: 4,
+  question: " ",
+  answer: "Hello",
+  options: [
+    "Idharammayilatho",
+    "Calling sahasra",
+    "Tik Tik Tik",
+    "Hello"
+  ]
+},
+   {
+  numb: 5,
+  question: " ",
+  answer: "Love story",
+  options: [
+    "Style",
+    "Love story",
+    "Happy days",
+    "Natyam"
+  ]
+},
+{
+  numb: 6,
+  question: " ",
+  answer: "Oopiri",
+  options: [
+    "Shyam Singha Roy",
+    "Kaashmora",
+    "Oopiri",
+    "Premam"
+  ]
+},
+{
+  numb: 7,
+  question: " ",
+  answer: "Fidha",
+  options: [
+    "Arya 2",
+    "Andhala Rakshasi",
+    "Fidha",
+    "Rx 100"
+  ]
+},
+{
+  numb: 8,
+  question: " ",
+  answer: "Age Of Ultron",
+  options: [
+    "Terminator",
+    "Robots",
+    "Thor: Love and Thunder",
+    "Age Of Ultron"
+  ]
+},
+{
+  numb: 9,
+  question: " ",
+  answer: "Nijam",
+  options: [
+    "Takkari donga",
+    "Jayam",
+    "Nijam",
+    "Sainikudu"
+  ]
+},
+{
+  numb: 10,
+  question: " ",
+  answer: "Kabaddi",
+  options: [
+    "Krishnashtami ",
+    "Kabaddi",
+    "Okkadu",
+    "Radha"
+  ]
+},
+{
+  numb: 11,
+  question: " ",
+  answer: "Bommarillu",
+  options: [
+    "Nana",
+    "Nannaku Prematho",
+    "Bommarillu",
+    "S/O Satyamurthy"
+  ]
+},
+{
+  numb: 12,
+  question: " ",
+  answer: "Pelli choopulu",
+  options: [
+    "Husharu",
+    "Pelli choopulu",
+    "Taxiwala",
+    "Ulavacharu biryani"
+  ]
+},
+{
+  numb: 13,
+  question: " ",
+  answer: "Snehithudu",
+  options: [
+    "Snehithudu",
+    "Rangam",
+    "Maharshi",
+    "Leader"
+  ]
+},
+{
+  numb: 14,
+  question: " ",
+  answer: "Spyder",
+  options: [
+    "Ninnu Kori",
+    "Mahanati",
+    "Spyder",
+    "Danger"
+  ]
+},
+{
+  numb: 15,
+  question: " ",
+  answer: "Happy Birthday",
+  options: [
+    "F3",
+    "Happy Deathday",
+    "Money money more money ",
+    "Happy Birthday"
+  ]
+},
+{
+  numb: 16,
+  question: " ",
+  answer: "Middle class melodies",
+  options: [
+    "Middle class melodies",
+    "Ulavacharu biryani",
+    "Premam",
+    "MCA"
+  ]
+},
+{
+  numb: 17,
+  question: " ",
+  answer: "Sahasam",
+  options: [
+    "Golimaar",
+    "Sahasam",
+    "Souryam",
+    "Lakshyam"
+  ]
+},
+{
+  numb: 18,
+  question: " ",
+  answer: "Raja the great",
+  options: [
+    "Raja the great",
+    "Power",
+    "Nippu",
+    "Touch chesi chudu"
+  ]
+},
+{
+  numb: 19,
+  question: " ",
+  answer: "Ala vaikunthapurramuloo",
+  options: [
+    "Iddharammayilatho",
+    "Badri",
+    "Madam",
+    "Ala vaikunthapurramuloo"
+  ]
+},
+{
+  numb: 20,
+  question: " ",
+  answer: "Lingaa",
+  options: [
+    "Bunny",
+    "Lingaa",
+    "Bagiradha",
+    "Sri anjaneyam"
+  ]
+},
+{
+  numb: 21,
+  question: " ",
+  answer: "Vedam",
+  options: [
+    "Dongodu",
+    "Drishyam",
+    "Vedam",
+    "Dhrona"
+  ]
+},
+{
+  numb: 22,
+  question: " ",
+  answer: "Nuvve kavali",
+  options: [
+    "Snehitudu",
+    "Nuvve kavali",
+    "Kerintha",
+    "Aanandham"
+  ]
+},
+{
+  numb: 23,
+  question: " ",
+  answer: "Prema katha chitram",
+  options: [
+    "Deyyam",
+    "Aa intlo",
+    "Antha:puram",
+    "Prema katha chitram"
+  ]
+},
+{
+  numb: 24,
+  question: " ",
+  answer: "Khaidi",
+  options: [
+    "Khaidi",
+    "Kaaki",
+    "Bheeshma",
+    "Pushpa the rise"
+  ]
+},
+{
+  numb: 25,
+  question: " ",
+  answer: "Mahanati",
+  options: [
+    "Mahanati",
+    "Abhinetri",
+    "Khadgam",
+    "Ok Bangaram"
+  ]
+},
+{
+  numb: 26,
+  question: " ",
+  answer: "Hit the first case",
+  options: [
+    "Ghani",
+    "Hit the first case",
+    "Lakshya",
+    "Liger"
+  ]
+},
+{
+  numb: 27,
+  question: " ",
+  answer: "Satyam",
+  options: [
+    "Katha",
+    "Satyam",
+    "Premakatha",
+    "Shyam singha roy"
+  ]
+},
+{
+  numb: 28,
+  question: " ",
+  answer: "Ninne premistha",
+  options: [
+    "Gaalipatam",
+    "Ninne premistha",
+    "Ave kallu",
+    "kanupapa"
+  ]
+},
+{
+  numb: 29,
+  question: " ",
+  answer: "Vakeel saab",
+  options: [
+    "Monagadu",
+    "Bheemla nayak",
+    "Temper",
+    "Vakeel saab"
+  ]
+},
+{
+  numb: 30,
+  question: " ",
+  answer: "F3",
+  options: [
+    "Paisa vasool",
+    "F3",
+    "Navvule navvulu",
+    "Jaathi ratnalu"
+  ]
+},
+{
+  numb: 31,
+  question: " ",
+  answer: "Devudu chesina manushulu",
+  options: [
+    "Devudu chesina manushulu",
+    "Amar akhbar anthoni",
+    "Kick",
+    "Khatarnak"
+  ]
+},
+{
+  numb: 32,
+  question: " ",
+  answer: "Shatamanam bhavati",
+  options: [
+    "Rangde",
+    "100% love",
+    "Shatamanam bhavati",
+    "Fidda"
+  ]
+},
+{
+  numb: 33,
+  question: " ",
+  answer: "Nannaku prematho",
+  options: [
+    "Nannaku prematho",
+    "Butterfly",
+    "Shayam singha roy",
+    "Manam"
+  ]
+},
+{
+  numb: 34,
+  question: " ",
+  answer: "Gruham",
+  options: [
+    "Ekkadiki pothavu chinnavada",
+    "Gruham",
+    "Aa intlo",
+    "Dheyam"
+  ]
+},
+{
+  numb: 35,
+  question: " ",
+  answer: "Chalo",
+  options: [
+    " Chal mohan ranga",
+    "Hello",
+    "Chalo",
+    "Run raja run"
+  ]
+},
+{
+  numb: 36,
+  question: " ",
+  answer: "Kshanam",
+  options: [
+    "Spy",
+    "Agent sai srinivasa athreya",
+    "Kshanam",
+    "Kanam"
+  ]
+},
+{
+  numb: 37,
+  question: " ",
+  answer: "Dj (Duvvada jagannadham)",
+  options: [
+    "Dj thillu",
+    "Gangstar",
+    "Krishnarjuna yuddham",
+    "Dj (Duvvada jagannadham)"
+  ]
+},
+{
+  numb: 38,
+  question: " ",
+  answer: "Baahubali-2: The Conclusion",
+  options: [
+    "Bimbisara",
+    "Baahubali-1: The Beginning",
+    "Baahubali-2: The Conclusion",
+    "Anaganaga o dheerudu"
+  ]
+},
+{
+  numb: 39,
+  question: " ",
+  answer: "Gentleman",
+  options: [
+    "Gentleman",
+    "Ashta chamma",
+    "Ala modalaindi",
+    "Pilla jamindhar"
+  ]
+},
+{
+  numb: 40,
+  question: " ",
+  answer: "Orey bujjiga",
+  options: [
+    "Nanna nenu naa boyfriends",
+    "Kumari 21F",
+    "Orey bujjiga",
+    "Eedo rakam aado rakam"
+  ]
+},
+{
+  numb: 41,
+  question: " ",
+  answer: "Yevade subramanyam",
+  options: [
+    "Keshava",
+    "Pelli choopulu",
+    "Dwaraka",
+    "Yevade subramanyam"
+  ]
+},
+{
+  numb: 42,
+  question: " ",
+  answer: "Bheemla nayak",
+  options: [
+    "RRR",
+    "Bheemla nayak",
+    "Jai lava kusa",
+    "Nayak"
+  ]
+},
+{
+  numb: 43,
+  question: " ",
+  answer: "Kathanayakudu",
+  options: [
+    "Kathanayakudu",
+    "Krishna",
+    "Karthikeya 2",
+    "Subramaniapuram"
+  ]
+},
+{
+  numb: 44,
+  question: " ",
+  answer: "Rocketry: The nambi effect",
+  options: [
+    "Antariksham 9000 KMPH",
+    "Rashmi rocket",
+    "Rocketry: The nambi effect",
+    "Adbhutham"
+  ]
+},
+{
+  numb: 45,
+  question: " ",
+  answer: "Major",
+  options: [
+    "Major",
+    "Sita ramam",
+    "Sarileru neekevvaru",
+    "The Ghazi attack"
+  ]
+},
+{
+  numb: 46,
+  question: " ",
+  answer: "Colour photo",
+  options: [
+    "Pushpa:The rise",
+    "Colour photo",
+    "Endukante premanta",
+    "Ready"
+  ]
+},
+{
+  numb: 47,
+  question: " ",
+  answer: "Radhe shyam",
+  options: [
+    "Kgf Chapter2",
+    "Titanic",
+    "Radhe shyam",
+    "Gangotri"
+  ]
+},
+{
+  numb: 48,
+  question: " ",
+  answer: "Nee sneham",
+  options: [
+    "Nee sneham",
+    "Vunnadhi okate zindagi",
+    "Sye",
+    "Snehamante idera"
+  ]
+},
+{
+  numb: 49,
+  question: " ",
+  answer: "Raja raja chora",
+  options: [
+    "Swami ra ra",
+    "Chanti",
+    "Hit",
+    "Raja raja chora"
+  ]
+},
+{
+  numb: 50,
+  question: " ",
+  answer: "Malleswari",
+  options: [
+    "Cinderella",
+    "Malleswari",
+    "Queen",
+    "Sita ramam"
+  ]
+},
+{
+  numb: 51,
+  question: " ",
+  answer: "Manmadhudu-2",
+  options: [
+    "Premam",
+    "Manmadhudu-2",
+    "Raju gari gadhi-2",
+    "Majili"
+  ]
+},
+{
+  numb: 52,
+  question: " ",
+  answer: "SwamyRara",
+  options: [
+    "SwamyRara",
+    "ChorBazaar",
+    "Raja Raja Chora",
+    "Dongata"
+  ]
+},
+{
+  numb: 53,
+  question: " ",
+  answer: "Virata Parvam",
+  options: [
+    "Virapan",
+    "narapa",
+    "Dhandupalem",
+    "Virata Parvam"
+  ]
+},
 ];
